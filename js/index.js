ef288071ed05416e922d7a631beb987d33a9efa7
Megat Iskandar Shahriman <shahriman.iskandar@gmail.com>
2022-07-14 15:10:03 +0800

    Add function to allow user to enter input
    Users will be prompted to determine the width and height of the Etch-a-Sketch.
    After the prompt a new grid, based on the users answers, will be generated.

--- js/index.js
+++ js/index.js
@@ -1,27 +1,44 @@
-let gridMain = document.querySelector('.gridContainer');
+const gridMain = document.getElementById('gridContainer');
+const gridButton = document.querySelector('.setBtn');
 
 
 //Create the inner <div>'s
-function createDivs() {
+function createDivs(gridSize) {
+    const widthHeight = 100 / gridSize;
     const div = document.createElement('div');
     div.classList.add('innerGrid');
+    div.style.width = `${widthHeight}%`;
+    div.style.height = `${widthHeight}%`;
     return div;
 }
 
 //Create the grid and append it to the main grid
-function createGrid() {
-    for(let i = 0; i < 256; i++) {
-        gridMain.appendChild(createDivs());
+function createGrid(gridSize) {
+    for(let i = 0; i < gridSize; i++) {
+        gridMain.appendChild(createDivs(Math.sqrt(gridSize)));
     }
 }
 
-createGrid();
-
 //Change div color
-
 gridMain.addEventListener('mouseover', function(e) {
     if(e.target.matches('.innerGrid')) {
         e.target.classList.add('gridColor');
     }
 });
+
+//Button function to set grid number
+gridButton.addEventListener('click', function() {
+    let gridSize = Number(prompt("How many square per side is your grid?"));
+    let totalGrid = gridSize * gridSize;
+
+    if(gridSize > 100) {
+        alert('This number is too big: choose a number below 100');
+    }else {
+        createGrid(totalGrid);
+    }
+});
+
+
+
+
 
